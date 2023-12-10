5c0947520b7aaf89bfa4e5074c9f443c0bd9321c
Braden Soller <braden.soller18@gmail.com>
2023-12-10 14:34:16 -0600

    removed header in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,7 @@ import DetailsPage from '../DetailsPage';
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+
       <Router>        
         <Route path="/" exact>
           <MovieList />
